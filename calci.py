35e7cdd7674f3325bf280dbc7cc9513c797406ce
yashu510 <92032651+yashu510@users.noreply.github.com>
2022-05-03 12:16:25 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,15 @@
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+def division(first_term, second_term):
+    return first_term / second_term
